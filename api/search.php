dd143664a35a480ebfe6359004ac522cfa91dee8
Silvester van der Leer <silvester@vdleer.nl>
2025-07-23 14:43:17 +0200

    api fix

--- api/search.php
+++ api/search.php
@@ -233,7 +233,8 @@ try {
             $searchQuery = str_replace('*', '%', $searchQuery);
             
             // Query that gets only active devices - using only hostname/alias fields
-            $sql = "SELECT a.hostname, 
+            $sql = "SELECT a.hostname, '' as ip_address, a.description, 
+                   a.created_at, a.updated_at, a.location
                    FROM access.devicesnew a 
                    LEFT JOIN reporting.acc_alias b ON UPPER(a.hostname) = UPPER(b.ccap_name)
                    WHERE (UPPER(a.hostname) LIKE UPPER(?) OR UPPER(b.alias) LIKE UPPER(?))
